6d706ba2e71b41467e9a1fa70b3be500a536d2f0
Edil Ashimov <edil@floatleft.tv>
2021-07-29 10:30:12 -0400

    - Uncomment the 'test_load_deliversConnectivityErrorOnClientError' test and make it pass

--- FeedAPIChallenge/Feed API/FeedsImageMapper.swift
+++ FeedAPIChallenge/Feed API/FeedsImageMapper.swift
@@ -9,32 +9,31 @@
 import Foundation
 
 final internal class FeedImageMapper {
-	
 	private struct Root: Decodable {
 		let items: [Image]
 		var feed: [FeedImage] {
 			return items.map { $0.item }
 		}
 	}
-	
+
 	private struct Image: Decodable {
 		public let id: UUID
 		public let description: String?
 		public let location: String?
 		public let url: URL
-		
+
 		var item: FeedImage {
 			return FeedImage(id: id,
-							 description: description,
-							 location: location,
-							 url: url)
+			                 description: description,
+			                 location: location,
+			                 url: url)
 		}
 	}
-	
+
 	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
 		guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData) }
-		
+
 		return .success(root.feed)
 	}
 }
